79b2a8163ff7b7157ef3ff6bccd2863db9b3ca5c
Fady <fnakhla23@gmail.com>
2024-10-23 04:40:45 +0000

    additional methods

--- src/graphneuralnets/message_passing/graph_matrix.py
+++ src/graphneuralnets/message_passing/graph_matrix.py
@@ -8,14 +8,31 @@ SquareDim = TypeVar("SquareDim", bound=int)
 
 
 class GraphMatrix(Generic[SquareDim]):
-    def __init__(self, adjacency_matrix: npt.NDArray[tuple[SquareDim, SquareDim]: np.uint]) -> None:
+    def __init__(
+        self, adjacency_matrix: npt.NDArray[tuple[SquareDim, SquareDim] :]
+    ) -> None:
         super().__init__()
         self.adjacency_matrix = adjacency_matrix
-        self.degree_matrix = GraphMatrix.to_degree_matrix(self.adjacency_matrix)
 
     @classmethod
-    def to_degree_matrix(cls, adjacency_matrix: npt.NDArray[tuple[SquareDim, SquareDim]: np.uint]) -> npt.NDArray[tuple[SquareDim, SquareDim]: np.uint]:
+    def to_degree_matrix(
+        cls, adjacency_matrix: npt.NDArray[tuple[SquareDim, SquareDim] :]
+    ) -> npt.NDArray[tuple[SquareDim, SquareDim] :]:
         degrees = adjacency_matrix.sum(axis=-1)
         degree_matrix = np.zeros_like(adjacency_matrix)
         np.einsum("...ii->...i", degree_matrix)[:] = degrees
         return degree_matrix
+
+    @classmethod
+    def to_convolution_form(
+        cls, matrix: npt.NDArray[tuple[SquareDim, SquareDim] :]
+    ) -> npt.NDArray[tuple[SquareDim, SquareDim] :]:
+        identity = np.eye(matrix.shape[-1], dtype=matrix.dtype)
+        return matrix + np.broadcast_to(identity, matrix.shape)
+
+    @property
+    def undirected_graph(self) -> "GraphMatrix":
+        symmetric_adjacency = np.logical_or(
+            self.adjacency_matrix, self.adjacency_matrix.transpose((-2, -1))
+        )
+        return GraphMatrix(symmetric_adjacency)
